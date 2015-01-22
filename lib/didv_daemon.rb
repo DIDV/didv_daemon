2725b5cc7428ba20bf56525b6bda58cb00090668
pelgrim <guskald@gmail.com>
2015-01-22 21:08:30 -0200

    can convert a lowercase-only word to a braille instance

--- lib/didv_daemon.rb
+++ lib/didv_daemon.rb
@@ -3,5 +3,6 @@ $:.unshift(File.dirname(__FILE__))
 require 'bundler/setup'
 require 'zip'
 require 'nokogiri'
+require 'yaml'
 require 'didv_daemon/epub'
 require 'didv_daemon/braille'
--- lib/didv_daemon/braille.rb
+++ lib/didv_daemon/braille.rb
@@ -16,6 +16,15 @@ module DIDV
       end
     end
 
+    def cells
+      cell_array = []
+      tmp_content = @content.chars
+      while tmp_content.any?
+        cell_array << tmp_content.shift(6).join
+      end
+      cell_array
+    end
+
     private
 
     def valid? content
@@ -29,6 +38,33 @@ module DIDV
       end
     end
 
+  end
+
+end
+
+module DIDV
+
+  UCHARS="[A-ZÀ-ÖØ-Ý]"
+  SCHARS="[,;:.'\"?!-]"
+  LCHARS="[a-zà-öø-ÿ]"
+  NCHARS="[0-9]"
+
+
+  def self.to_braille word
+    if word.match /\A(#{LCHARS})+\z/
+      content = ""
+      word.each_char { |char| content << char_to_braille(char) }
+      Braille.new(content)
+    else
+      false
+    end
+  end
+
+  private
+
+  def self.char_to_braille char
+    braille_yml = YAML::load_file("lib/didv_daemon/braille.yml")
+    braille_yml['non_numeric'][char]
   end
 
 end
