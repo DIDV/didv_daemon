25b7bea5c5f9d99d3367c0fd0f0b9375555a523a
pelgrim <guskald@gmail.com>
2015-01-23 14:25:32 -0200

    fixed ink_text specs

--- specs/ink_text.rb
+++ specs/ink_text.rb
@@ -1,11 +1,13 @@
 require_relative '../lib/didv_daemon' unless defined?(DIDV)
 
-describe DIDV::InkText, "new" do
+describe DIDV::InkText, "to_braille" do
 
   it "returns the expected braille to a lower case letters-only string" do
     ink_text = DIDV::InkText.new("my text")
-    expected = "101100101111000000011110100010101101011110"
-    expect( ink_text.to_braille.content ).to eq(expected)
+    foo = "101100101111000000011110100010101101011110"
+    bar = ink_text.to_braille.content
+    puts bar
+    expect( bar ).to eq( foo )
   end
 
   it "returns the expected braille to numeric string" do
