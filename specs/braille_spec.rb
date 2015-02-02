0725cb56cb8fecf03b54f603550f34d665c472ba
pelgrim <guskald@gmail.com>
2015-02-01 23:05:04 -0200

    corrected test specs

--- specs/braille_spec.rb
+++ specs/braille_spec.rb
@@ -2,11 +2,11 @@ require_relative '../lib/didv_daemon' unless defined?(DIDV)
 
 describe DIDV::Braille, "new" do
 
-  it "should return a Braille object to a valid content" do
+  it "should return a Braille object to a nil content" do
     expect(DIDV::Braille.new(nil)).to be_a(DIDV::Braille)
   end
 
-  it "should return a Braille object to a nil content" do
+  it "should return a Braille object to a valid content" do
     expect(DIDV::Braille.new("100000\n")).to be_a(DIDV::Braille)
   end
 
