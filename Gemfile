2296104f7ca98a08aceffc960c6501e5fe0b3ae6
pelgrim <guskald@gmail.com>
2015-04-17 22:17:22 -0300

    included state_machine gem

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.1.2'
 gem 'nokogiri', '~>1.6.5'
 gem 'rubyzip', '~>1.1.6'
 gem 'eventmachine', '~>1.0.4'
+gem 'state_machine'
 
 group :development do
   gem 'rspec'
--- lib/didv_daemon.rb
+++ lib/didv_daemon.rb
@@ -4,11 +4,13 @@ require 'bundler/setup'
 require 'zip'
 require 'nokogiri'
 require 'yaml'
+require 'state_machine'
 require 'didv_daemon/conf'
 require 'didv_daemon/epub'
 require 'didv_daemon/braille'
 require 'didv_daemon/ink_text'
 require 'didv_daemon/element'
+require 'didv_daemon/didv_ux'
 
 module DIDV
 
