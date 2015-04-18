2389a443638d36c74eb5009ecf55b6ab139a439b
pelgrim <guskald@gmail.com>
2015-04-18 10:11:24 -0300

    code sanitize

--- lib/didv_daemon.rb
+++ lib/didv_daemon.rb
@@ -9,11 +9,4 @@ require 'didv_daemon/conf'
 require 'didv_daemon/epub'
 require 'didv_daemon/braille'
 require 'didv_daemon/ink_text'
-require 'didv_daemon/element'
 require 'didv_daemon/didv_ux'
-
-module DIDV
-
-  
-
-end
