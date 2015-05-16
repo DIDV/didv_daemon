19adc77119ab97ad8398e31e420116937c1cd76e
pelgrim <guskald@gmail.com>
2015-05-16 15:55:27 -0300

    daemon final

--- bin/didv_daemon_init.rb
+++ bin/didv_daemon_init.rb
@@ -3,7 +3,9 @@ require 'daemons'
 script = "#{File.dirname(__FILE__)}/didv_daemon.rb"
 options = {
   app_name: "didv_daemon",
-  log_dir: "/home/didv",
+  dir_mode: :normal,
+  dir: "/home/didv/pid",
+  log_dir: "/home/didv/log",
   log_output: true
 }
 
